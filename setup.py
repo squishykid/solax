fd94c719274bc57336ec53a4035bbd2f731f23a0
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2020-06-09 22:38:29 +1000

    bump version to 0.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="solax",
-    version="0.2.2",
+    version="0.2.3",
     author="Robin Wohlers-Reichel",
     author_email="me@robinwr.com",
     description="Solax inverter API client",
