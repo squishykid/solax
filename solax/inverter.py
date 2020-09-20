8703b1833c89a2780b4a15531e7e040701f105d3
Robin Wohlers-Reichel <me@robinwohlers.com>
2020-09-20 17:08:29 +1000

    add x1-boost-air-mini #21 and X3-Hybrid-G3 #23 (#24)

--- solax/inverter.py
+++ solax/inverter.py
@@ -89,6 +89,15 @@ async def discover(host, port) -> Inverter:
     raise DiscoveryError(msg)
 
 
+def startswith(something):
+    def inner(actual):
+        if isinstance(actual, str):
+            if actual.startswith(something):
+                return actual
+        raise Invalid(f"{str(actual)} does not start with {something}")
+    return inner
+
+
 class XHybrid(Inverter):
     """
     Tested with:
@@ -195,13 +204,18 @@ class InverterPost(Inverter):
 
 class X3(InverterPost):
     __schema = vol.Schema({
-        vol.Required('type'): str,
+        vol.Required('type'): vol.All(
+            str,
+            startswith("X3-")
+        ),
         vol.Required('SN'): str,
         vol.Required('ver'): str,
         vol.Required('Data'): vol.Schema(
             vol.All(
                 [vol.Coerce(float)],
-                vol.Length(min=103, max=103)
+                vol.Any(
+                    vol.Length(min=102, max=102),
+                    vol.Length(min=103, max=103)),
                 )
             ),
         vol.Required('Information'): vol.Schema(
@@ -265,15 +279,6 @@ class X3(InverterPost):
     @classmethod
     def schema(cls):
         return cls.__schema
-
-
-def startswith(something):
-    def inner(actual):
-        if isinstance(actual, str):
-            if actual.startswith(something):
-                return actual
-        raise Invalid(f"{str(actual)} does not start with {something}")
-    return inner
 
 
 class X1(InverterPost):
@@ -343,5 +348,59 @@ class X1(InverterPost):
         return cls.__schema
 
 
+class X1Mini(InverterPost):
+    __schema = vol.Schema({
+        vol.Required('type'): vol.All(
+            str,
+            startswith("X1-")
+        ),
+        vol.Required('SN'): str,
+        vol.Required('ver'): str,
+        vol.Required('Data'): vol.Schema(
+            vol.All(
+                [vol.Coerce(float)],
+                vol.Length(min=69, max=69),
+            )
+        ),
+        vol.Required('Information'): vol.Schema(
+            vol.All(
+                vol.Length(min=9, max=9)
+                )
+            ),
+    }, extra=vol.REMOVE_EXTRA)
+
+    __sensor_map = {
+        'PV1 Current':                (0, 'A'),
+        'PV2 Current':                (1, 'A'),
+        'PV1 Voltage':                (2, 'V'),
+        'PV2 Voltage':                (3, 'V'),
+
+        'Output Current':             (4, 'A'),
+        'Network Voltage':            (5, 'V'),
+        'AC Power':                   (6, 'W'),
+
+        'Inverter Temperature':       (7, 'C'),
+        'Today\'s Energy':            (8, 'kWh'),
+        'Total Energy':               (9, 'kWh'),
+        'Exported Power':             (10, 'W'),
+        'PV1 Power':                  (11, 'W'),
+        'PV2 Power':                  (12, 'W'),
+
+        'Total Feed-in Energy':       (41, 'kWh'),
+        'Total Consumption':          (42, 'kWh'),
+
+        'Power Now':                  (43, 'W'),
+        'Grid Frequency':             (50, 'Hz'),
+    }
+
+    @classmethod
+    def sensor_map(cls):
+        return cls.__sensor_map
+
+    @classmethod
+    def schema(cls):
+        return cls.__schema
+
+
 # registry of inverters
-REGISTRY = [XHybrid, X3, X1]
+REGISTRY = [XHybrid, X3, X1, X1Mini]
