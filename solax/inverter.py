5b58e370de3336b3417fcd42f7854a71ad404c95
Robin Wohlers-Reichel <me@robinwr.com>
2021-08-17 13:20:13 +1000

    X1Mini fix #36 (#41)
    Co-authored-by: Bo Biene <23037659+BoBiene@users.noreply.github.com>

--- solax/inverter.py
+++ solax/inverter.py
@@ -521,5 +521,71 @@ class X1Mini(InverterPost):
         return cls.__schema
 
 
+class X1MiniV34(InverterPost):
+    __schema = vol.Schema({
+        vol.Required('type', 'type'): vol.All(int, 4),
+        vol.Required('sn',): str,
+        vol.Required('ver'): str,
+        vol.Required('Data'): vol.Schema(
+            vol.All(
+                [vol.Coerce(float)],
+                vol.Any(
+                    vol.Length(min=69, max=69),
+                    vol.Length(min=200, max=200),
+                )
+            )
+        ),
+        vol.Required('Information'): vol.Schema(
+            vol.Any(
+                vol.Length(min=9, max=9),
+                vol.Length(min=10, max=10)
+            )
+        ),
+    }, extra=vol.REMOVE_EXTRA)
+
+    __sensor_map = {
+        'Network Voltage':            (0, 'V', div10),
+        'Output Current':             (1, 'A', div10),
+        'AC Power':                   (2, 'W'),
+        'PV1 Voltage':                (3, 'V', div10),
+        'PV2 Voltage':                (4, 'V', div10),
+        'PV1 Current':                (5, 'A', div10),
+        'PV2 Current':                (6, 'A', div10),
+        'PV1 Power':                  (7, 'W'),
+        'PV2 Power':                  (8, 'W'),
+        'Grid Frequency':             (9, 'Hz', div100),
+        'Total Energy':               (11, 'kWh', div10),
+        'Today\'s Energy':            (13, 'kWh', div10),
+        'Total Feed-in Energy':       (41, 'kWh', div10),
+        'Total Consumption':          (42, 'kWh', div10),
+        'Power Now':                  (43, 'W', div10),
+    }
+
+    @classmethod
+    def sensor_map(cls):
+        """
+        Return sensor map
+        """
+        sensors = {}
+        for name, (idx, unit, *_) in cls.__sensor_map.items():
+            sensors[name] = (idx, unit)
+        return sensors
+
+    @classmethod
+    def postprocess_map(cls):
+        """
+        Return postprocessing map
+        """
+        sensors = {}
+        for name, (_, _, *processor) in cls.__sensor_map.items():
+            if processor:
+                sensors[name] = processor[0]
+        return sensors
+
+    @classmethod
+    def schema(cls):
+        return cls.__schema
+
+
 # registry of inverters
-REGISTRY = [XHybrid, X3, X3V34, X1, X1Mini]
+REGISTRY = [XHybrid, X3, X3V34, X1, X1Mini, X1MiniV34]
