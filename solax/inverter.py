cbc93ebc5615be1b0190c5e0d6463d8f6a68fb05
JJ Macalinao <jessie.macalinao@cai.io>
2021-12-14 00:02:09 +0800

    Add support for X1-Smart

--- solax/inverter.py
+++ solax/inverter.py
@@ -587,5 +587,100 @@ class X1MiniV34(InverterPost):
         return cls.__schema
 
 
+class X1Smart(InverterPost):
+    """
+    X1-Smart with Pocket WiFi v2.033.20
+    Includes X-Forwarded-For for direct LAN API access
+    """
+    __schema = vol.Schema({
+        vol.Required('type', 'type'): vol.All(int, 8),
+        vol.Required('sn',): str,
+        vol.Required('ver'): str,
+        vol.Required('Data'): vol.Schema(
+            vol.All(
+                [vol.Coerce(float)],
+                vol.Length(min=200, max=200),
+            )
+        ),
+        vol.Required('Information'): vol.Schema(
+            vol.All(
+                vol.Length(min=8, max=8)
+            )
+        ),
+    }, extra=vol.REMOVE_EXTRA)
+
+    __sensor_map = {
+        'Network Voltage':            (0, 'V', div10),
+        'Output Current':             (1, 'A', div10),
+        'AC Power':                   (2, 'W'),
+        'PV1 Voltage':                (3, 'V', div10),
+        'PV2 Voltage':                (4, 'V', div10),
+        'PV1 Current':                (5, 'A', div10),
+        'PV2 Current':                (6, 'A', div10),
+        'PV1 Power':                  (7, 'W'),
+        'PV2 Power':                  (8, 'W'),
+        'Grid Frequency':             (9, 'Hz', div100),
+        'Total Energy':               (11, 'kWh', div10),
+        'Today\'s Energy':            (13, 'kWh', div10),
+        'Inverter Temperature':       (39, 'C'),
+        'Exported Power':             (48, 'W', to_signed),
+        'Total Feed-in Energy':       (50, 'kWh', div100),
+        'Total Consumption':          (52, 'kWh', div100),
+    }
+
+    @classmethod
+    async def make_request(cls, host, port=80, pwd=''):
+        headers = {'X-Forwarded-For': '5.8.8.8'}
+        if not pwd:
+            base = 'http://{}:{}/?optType=ReadRealTimeData'
+            url = base.format(host, port)
+        else:
+            base = 'http://{}:{}/?optType=ReadRealTimeData&pwd={}&'
+            url = base.format(host, port, pwd)
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, headers=headers) as req:
+                resp = await req.read()
+        raw_json = resp.decode("utf-8")
+        json_response = json.loads(raw_json)
+        response = {}
+        try:
+            response = cls.schema()(json_response)
+        except (Invalid, MultipleInvalid) as ex:
+            _ = humanize_error(json_response, ex)
+            # print(_)
+            raise
+        return InverterResponse(
+            data=cls.map_response(response['Data']),
+            serial_number=response['sn'],
+            version=response['ver'],
+            type=response['type']
+        )
+
+    @classmethod
+    def sensor_map(cls):
+        """
+        Return sensor map
+        """
+        sensors = {}
+        for name, (idx, unit, *_) in cls.__sensor_map.items():
+            sensors[name] = (idx, unit)
+        return sensors
+
+    @classmethod
+    def postprocess_map(cls):
+        """
+        Return postprocessing map
+        """
+        sensors = {}
+        for name, (_, _, *processor) in cls.__sensor_map.items():
+            if processor:
+                sensors[name] = processor[0]
+        return sensors
+
+    @classmethod
+    def schema(cls):
+        return cls.__schema
+
+
 # registry of inverters
-REGISTRY = [XHybrid, X3, X3V34, X1, X1Mini, X1MiniV34]
+REGISTRY = [XHybrid, X3, X3V34, X1, X1Mini, X1MiniV34, X1Smart]
