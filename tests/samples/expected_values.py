3cd3b822bbff0f6c8c41af6f033fdef4fc1e443d
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2022-05-10 11:14:36 +1200

    Update expected reponse keys

--- tests/samples/expected_values.py
+++ tests/samples/expected_values.py
@@ -332,22 +332,22 @@ X1_SMART_VALUES = {
 }
 
 X1_BOOST_VALUES = {
-    "Network Voltage": 239.6,
-    "Output Current": 12.6,
-    "AC Power": 2956,
+    "AC Voltage": 239.6,
+    "AC Output Current": 12.6,
+    "AC Output Power": 2956,
     "PV1 Voltage": 432.9,
     "PV2 Voltage": 233.8,
     "PV1 Current": 4.1,
     "PV2 Current": 5.7,
     "PV1 Power": 1777,
     "PV2 Power": 1336,
-    "Grid Frequency": 60.21,
-    "Total Energy": 121.5,
-    "Today's Energy": 13.7,
+    "AC Frequency": 60.21,
+    "Total Generated Energy": 121.5,
+    "Today's Generated Energy": 13.7,
     "Inverter Temperature": 43,
     "Exported Power": 10,
-    "Total Feed-in Energy": 0.87,
-    "Total Consumption": 81.84,
+    "Total Export Energy": 0.87,
+    "Total Import Energy": 81.84,
 }
 
 QVOLTHYBG33P_VALUES = {
