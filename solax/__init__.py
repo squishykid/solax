599d794d599b54e1a4e826627e74358a6b712364
Niclas Kühnapfel <niclasku@users.noreply.github.com>
2023-06-19 21:01:34 +0200

    Fix deprecation warning by using new API of async-timeout module (#123)

--- solax/__init__.py
+++ solax/__init__.py
@@ -2,7 +2,7 @@
 import asyncio
 import logging
 
-import async_timeout
+from async_timeout import timeout
 
 from solax.discovery import discover
 from solax.inverter import Inverter, InverterResponse
@@ -22,7 +22,7 @@ async def rt_request(inv: Inverter, retry, t_wait=0) -> InverterResponse:
     new_wait = (t_wait * 2) + 5
     retry = retry - 1
     try:
-        with async_timeout.timeout(REQUEST_TIMEOUT):
+        async with timeout(REQUEST_TIMEOUT):
             return await inv.get_data()
     except asyncio.TimeoutError:
         if retry > 0:
