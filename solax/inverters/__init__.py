547d01b8c5e5687b42368485f64e6a7037e025ca
gustavoio <gustavo.pinho.pt@gmail.com>
2023-09-13 19:03:38 +0100

    Ad support for solax x1-hybrid-gen5

--- solax/inverters/__init__.py
+++ solax/inverters/__init__.py
@@ -2,6 +2,7 @@ from .qvolt_hyb_g3_3p import QVOLTHYBG33P
 from .x1 import X1
 from .x1_boost import X1Boost
 from .x1_hybrid_gen4 import X1HybridGen4
+from .x1_hybrid_gen5 import X1HybridGen5
 from .x1_mini import X1Mini
 from .x1_mini_v34 import X1MiniV34
 from .x1_smart import X1Smart
@@ -23,5 +24,6 @@ __all__ = [
     "X3",
     "X1Boost",
     "X1HybridGen4",
+    "X1HybridGen5",
     "X3MicProG2",
 ]
