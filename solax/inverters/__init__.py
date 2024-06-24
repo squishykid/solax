90d98e89de5eb44e80249d383b79aad4d9229d46
Daniel Weeber <mail@daniel-weeber.de>
2024-06-24 22:42:49 +0200

    fix isort

--- solax/inverters/__init__.py
+++ solax/inverters/__init__.py
@@ -6,12 +6,12 @@ from .x1_mini import X1Mini
 from .x1_mini_v34 import X1MiniV34
 from .x1_smart import X1Smart
 from .x3 import X3
+from .x3_evc import X3EVC
 from .x3_hybrid_g4 import X3HybridG4
 from .x3_mic_pro_g2 import X3MicProG2
 from .x3_ultra import X3Ultra
 from .x3_v34 import X3V34
 from .x_hybrid import XHybrid
-from .x3_evc import X3EVC
 
 __all__ = [
     "QVOLTHYBG33P",
