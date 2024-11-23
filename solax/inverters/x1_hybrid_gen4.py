28ef56ad81bec49fbcd5d601cf5b58d7b56e58fd
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2024-11-23 16:43:24 +0200

    Fix Battery Power using unsigned int (X1 Gen4) (#162)

--- solax/inverters/x1_hybrid_gen4.py
+++ solax/inverters/x1_hybrid_gen4.py
@@ -49,7 +49,7 @@ class X1HybridGen4(Inverter):
             "On-grid daily yield": (13, DailyTotal(Units.KWH), div10),
             "Battery voltage": (14, Units.V, div100),
             "Battery current": (15, Units.A, div100),
-            "Battery power": (16, Units.W),
+            "Battery power": (16, Units.W, to_signed),
             "Battery temperature": (17, Units.C),
             "Battery SoC": (18, Units.PERCENT),
             "Inverter Temperature": (26, Units.C),
