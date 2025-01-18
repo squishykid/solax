0a41d7d72bf1627f8cfe470a858ef19fc0c67bbd
Robin Wohlers-Reichel <me@robinwr.com>
2025-01-18 11:29:19 +0100

    fix expected values
    Signed-off-by: Robin Wohlers-Reichel <me@robinwr.com>

--- tests/samples/expected_values.py
+++ tests/samples/expected_values.py
@@ -448,22 +448,22 @@ X1_MINI_G4_VALUES = {
 }
 
 X1_SMART_FW_V3_VALUES = {
-    "Network Voltage": 239.6,
-    "Output Current": 12.6,
-    "AC Power": 2956,
-    "PV1 Voltage": 432.9,
-    "PV2 Voltage": 233.8,
-    "PV1 Current": 4.1,
-    "PV2 Current": 5.7,
-    "PV1 Power": 1777,
-    "PV2 Power": 1336,
-    "Grid Frequency": 60.21,
-    "Total Energy": 121.5,
-    "Today's Energy": 13.7,
-    "Inverter Temperature": 43,
-    "Exported Power": 10,
-    "Total Feed-in Energy": 0.87,
-    "Total Consumption": 81.84,
+    "Network Voltage": 242.6,
+    "Output Current": 2.0,
+    "AC Power": 103.0,
+    "PV1 Voltage": 347.0,
+    "PV2 Voltage": 110.0,
+    "PV1 Current": 0.2,
+    "PV2 Current": 0.1,
+    "PV1 Power": 93.0,
+    "PV2 Power": 20.0,
+    "Grid Frequency": 50.04,
+    "Total Energy": 2720.7,
+    "Today's Energy": 35.3,
+    "Inverter Temperature": 30.0,
+    "Exported Power": -1920.0,
+    "Total Feed-in Energy": 377.44,
+    "Total Consumption": 329.84,
 }
 
 X1_SMART_VALUES = {
