7f40dbeb1cfafe010fbca33dc6a43c6aa93f0a1f
Jakub Darmach <jakub@stackhpc.com>
2025-01-20 16:30:16 +0100

    Removed text from Battery mode text.

--- solax/inverters/x3_hybrid_g4.py
+++ solax/inverters/x3_hybrid_g4.py
@@ -130,7 +130,7 @@ class X3HybridG4(Inverter):
                 div10,
             ),
             # "Battery mode": (168, Units.NONE), # Only use the index once due to HA uids
-            "Battery mode text": (168, Units.NONE, X3HybridG4._decode_battery_mode),
+            "Battery mode": (168, Units.NONE, X3HybridG4._decode_battery_mode),
             "Battery Voltage": (pack_u16(169, 170), Units.V, div100),
         }
 
